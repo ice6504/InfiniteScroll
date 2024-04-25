33f345e09f69f2e318ca74cbd77c7fed6b277127
ice6504 <aphinan6504@gmail.com>
2024-04-25 23:13:20 +0700

    Card Detail

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,12 +4,36 @@ function Card(props) {
   const { data } = props;
   return (
     <>
-      <div className="card h-96 bg-secondary text-primary-content shadow-xl">
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">{data.description}</h2>
-          <div className="card-actions">
-            <a className="btn btn-primary" href={data.url} target="_blank">
-              See More
+      <div className="card bg-secondary text-primary-content shadow-xl relative">
+        <div className="absolute inset-x-5 top-4 text-xs font-semibold text-primary uppercase line-clamp-1">
+          {data.topics
+            .map((topic, index) => {
+              return <span key={index}>{topic + " "}</span>;
+            })
+            }
+        </div>
+        <div className="card-body items-center gap-3">
+          <div className=" w-full grid place-items-center text-center h-10 sm:h-36">
+            <h2 className="card-title tracking-tight text-2xl">{data.description}</h2>
+          </div>
+          <div className="card-actions flex-col w-full gap-3">
+            <p>
+              Type :{" "}
+              {data.types.map((type) => {
+                return type + " ";
+              })}
+            </p>
+            <div className="flex gap-1 flex-wrap items-end">
+              {data.levels.map((topic, index) => {
+                return (
+                  <div key={index} className="badge">
+                    {topic}
+                  </div>
+                );
+              })}
+            </div>
+            <a className="btn btn-primary " href={data.url} target="_blank">
+              Learn More
             </a>
           </div>
         </div>
